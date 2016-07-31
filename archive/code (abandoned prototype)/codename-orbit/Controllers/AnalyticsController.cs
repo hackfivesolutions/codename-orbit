c12a0e0bb9f8bb28e362f4fe0b945729f91bf734
hack5 <hackfivesolutions@gmail.com>
2016-07-31 16:51:04 +0930

    archival of incomplete code

---
+++ archive/code (abandoned prototype)/codename-orbit/Controllers/AnalyticsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codename_orbit.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        // GET: Analytics
+        public ActionResult Index()
+        {
+            return RedirectToAction("Overview", "Analytics", null);
+        }
+
+        public ActionResult Overview()
+        {
+            return View();
+        }
+
+        public ActionResult Analytics()
+        {
+            return View();
+        }
+
+        public ActionResult Simulation()
+        {
+            return View();
+        }
+    }
+}
